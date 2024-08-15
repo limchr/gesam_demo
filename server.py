ac2ff032c73872f76e6ee414d4364ba257ff04f7
Christian Limberg <cnlimberg@gmail.com>
2024-08-15 15:30:31 +0900

    updated reference bibtex html element

--- server.py
+++ server.py
@@ -5,8 +5,15 @@ import socketserver
 
 PORT = 8080
 
-handler = lambda *args, **kwargs: http.server.SimpleHTTPRequestHandler(*args, directory=__file__[:__file__.rfind('/')], **kwargs)
+
+pd = __file__.rfind('/')
+if pd == -1:
+    pd = __file__.rfind('\\')
+dir = __file__[:pd]
+
+
+handler = lambda *args, **kwargs: http.server.SimpleHTTPRequestHandler(*args, directory=dir, **kwargs)
 
 with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print(f"Serving on port {PORT}")
+    print(f"Serving on port {PORT}, {dir}")
     httpd.serve_forever()
